0515fdc2ddaf7f23482f52a448c89e76366d08df
izzul-ali <izzulmaali3@gmail.com>
2022-12-23 22:16:06 +0700

    feat: add grid layout for products card

--- src/components/home/product-list.tsx
+++ src/components/home/product-list.tsx
@@ -4,9 +4,9 @@ export default function ProductList() {
   return (
     <>
       <div className="mt-10 flex justify-between items-center">
-        <h2 className="text-xl font-medium">Special For You</h2>
+        <h2 className="text-xl sm:text-2xl font-medium">Special For You</h2>
       </div>
-      <div className="mt-5 grid grid-cols-2 gap-x-2 gap-y-5 place-content-between">
+      <div className="mt-5 grid grid-cols-2 sm:grid-cols-3 gap-x-2 gap-y-5 place-content-between">
         {[...Array(10)].map((_, i) => (
           <ProductCard key={i} />
         ))}
