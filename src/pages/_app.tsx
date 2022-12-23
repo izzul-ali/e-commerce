0a2297ccedef12f3fcb86100653ebde25060944e
izzul-ali <izzulmaali3@gmail.com>
2022-12-23 14:46:01 +0700

    feat: add main layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import { AppProps } from 'next/app'
+import Layout from '~/components/layout'
 import '~/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
