b7688f29ac8c0ba889372d5b6a7e42c45454766f
izzul-ali <izzulmaali3@gmail.com>
2022-12-23 22:16:42 +0700

    feat: add max width

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import { AiOutlineShoppingCart, AiOutlineHeart } from 'react-icons/ai'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <main className="p-5">
+    <main className="p-5 max-w-4xl mx-auto">
       <div className="absolute bg-violet-600 left-0 right-0 top-0 h-52 -z-20 rounded-b-3xl"></div>
       <header className="flex justify-between">
         <h1 className="text-2xl text-white font-medium">E-Commerce</h1>
