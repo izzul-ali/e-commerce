5d0be70b7f5a38a5225ad7ef20fca717dddfc4f5
izzul-ali <izzulmaali3@gmail.com>
2022-12-24 23:13:22 +0700

    chore: setup react-query config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,29 @@
 import { AppProps } from 'next/app'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Layout from '~/components/layout'
 import '~/styles/globals.css'
 
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 1,
+      staleTime: 1000 * 60 * 60 * 1,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: 1
+    }
+  }
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
