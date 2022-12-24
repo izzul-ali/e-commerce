a18f6d26614f5c4abdad4ed8ccf4de34e4d50994
izzul-ali <izzulmaali3@gmail.com>
2022-12-24 23:08:41 +0700

    feat: add navigation link

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,22 +1,27 @@
+import Link from 'next/link'
 import { ReactNode } from 'react'
 import { AiOutlineShoppingCart, AiOutlineHeart } from 'react-icons/ai'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <main className="p-5 max-w-4xl mx-auto">
-      <div className="absolute bg-violet-600 left-0 right-0 top-0 h-52 -z-20 rounded-b-3xl"></div>
+      <div className="absolute bg-violet-600 left-0 right-0 top-0 h-52 -z-20 rounded-b-2xl"></div>
       <header className="flex justify-between">
         <h1 className="text-2xl text-white font-medium">E-Commerce</h1>
         <div className="flex items-center gap-x-4 [&>button]:outline-none">
-          <button>
-            <AiOutlineShoppingCart className="w-6 h-fit fill-slate-50" />
-          </button>
-          <button>
-            <AiOutlineHeart className="w-6 h-fit fill-slate-50" />
-          </button>
+          <Link href={'/'}>
+            <a>
+              <AiOutlineShoppingCart className="w-6 h-fit fill-slate-50" />
+            </a>
+          </Link>
+          <Link href={'/'}>
+            <a>
+              <AiOutlineHeart className="w-6 h-fit fill-slate-50" />
+            </a>
+          </Link>
         </div>
       </header>
-      <div className="mt-16">{children}</div>
+      <div className="mt-14">{children}</div>
     </main>
   )
 }
