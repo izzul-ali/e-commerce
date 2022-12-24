dfb9ed742fcd2d15c9086936da28e2fff52d37a7
izzul-ali <izzulmaali3@gmail.com>
2022-12-24 23:03:21 +0700

    feat: add skleton loading

---
+++ src/components/fallback/hero-card.tsx
@@ -0,0 +1,12 @@
+export default function HeroCardLoading() {
+  return (
+    <div className="grid grid-cols-2 gap-3 w-full h-32 px-1">
+      <div className="rounded h-full w-full object-cover bg-gray-500 animate-pulse"></div>
+      <div className="">
+        <h3 className="text-lg mb-2 font-medium line-clamp-3 py-2 w-full bg-gray-500 animate-pulse rounded-sm"></h3>
+        <h3 className="text-lg font-medium line-clamp-3 py-2 w-full bg-gray-500 animate-pulse rounded-sm"></h3>
+        <button className="mt-5 px-5 py-2 outline-none rounded bg-gray-500 animate-pulse"></button>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/skletons/skleton-product-card.tsx
@@ -0,0 +1,16 @@
+export default function ProductCardLoading() {
+  return (
+    <div className="h-[19rem] sm:h-[20rem] shadow-md w-full border border-gray-300 hover:border-violet-500 transition duration-300 rounded-md overflow-hidden">
+      <div className="w-full h-44 object-contain rounded-t bg-gray-500 animate-pulse"></div>
+      <div className="p-2 space-y-3">
+        <h3 className="text-sm sm:text-base font-medium line-clamp-2 py-2 px-6 rounded bg-gray-500 animate-pulse"></h3>
+        <h3 className="text-sm sm:text-base font-medium line-clamp-2 py-2 px-6 rounded bg-gray-500 animate-pulse"></h3>
+        <p className="text-xs bg-gray-500 animate-pulse py-2 px-6  w-fit rounded-md"></p>
+        <div className="flex justify-between items-center ">
+          <p className="font-medium px-6 py-2 bg-gray-500 animate-pulse rounded"></p>
+          <button className="outline-none px-6 py-2 bg-gray-500 animate-pulse rounded"></button>
+        </div>
+      </div>
+    </div>
+  )
+}
