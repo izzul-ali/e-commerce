241629ea64287964bd8ee9ed420d6e2d9d3c75a1
izzul-ali <izzulmaali3@gmail.com>
2022-12-27 17:41:42 +0700

    feat: using loading components before product display and handling errors

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,7 +1,7 @@
 import Carousel, { Settings } from 'react-slick'
 import { useProducts } from '~/store/products'
 import HeroCard from '../hero-card'
-import HeroCardLoading from '../skletons/loading-hero-card'
+import HeroCardLoading from '../fallback/skleton-hero-card'
 import '~/slick-carousel/slick/slick.css'
 import '~/slick-carousel/slick/slick-theme.css'
 
@@ -17,15 +17,15 @@ const carouselSetting: Settings = {
 }
 
 export default function Hero() {
-  const { data: products, isLoading, isRefetching } = useProducts()
+  const { data: products, isLoading, isRefetching, isError } = useProducts()
 
   return (
     <div className="w-full h-40 sm:h-52 bg-white px-4 hover:shadow-xl shadow-lg rounded-lg flex justify-center items-center">
-      {isLoading || isRefetching ? (
+      {isLoading || isRefetching || isError ? (
         <HeroCardLoading />
       ) : (
         <Carousel {...carouselSetting} className="h-fit w-full">
-          {products.slice(8, 12).map((v, i) => (
+          {products?.slice(8, 12).map((v, i) => (
             <HeroCard key={i} img={v.image} title={v.title} />
           ))}
         </Carousel>
--- src/components/home/product-list.tsx
+++ src/components/home/product-list.tsx
@@ -1,32 +1,37 @@
 import Link from 'next/link'
 import { useProducts } from '~/store/products'
-import ProductCardLoading from '../skletons/skleton-product-card'
+import { prices } from '~/constant/price'
+import ErrorProduct from '../fallback/error-product'
+import ProductCardLoading from '../fallback/skleton-product-card'
 import ProductCard from '../product-card'
 
 export default function ProductList() {
-  const { data: products, isLoading, isRefetching } = useProducts()
+  const { data: products, isLoading, isRefetching, isError } = useProducts()
 
   return (
     <>
       <div className="mt-10 flex justify-between items-center">
-        <h2 className="text-xl sm:text-2xl font-medium">Special For You</h2>
+        <h2 className="text-xl sm:text-2xl font-semibold">Special For You</h2>
       </div>
-
       <div className="mt-5 grid grid-cols-2 sm:grid-cols-3 gap-x-2 gap-y-5 place-content-between">
-        {isLoading || isRefetching
-          ? [...Array(4)].map((_, i) => <ProductCardLoading key={i} />)
-          : products?.map((v, i) => (
-              <Link href={`/product/${v.id}`} key={i}>
-                <a key={i}>
-                  <ProductCard
-                    img={v.image}
-                    price={String(v.price)}
-                    rating={String(v.rating.rate)}
-                    title={v.title}
-                  />
-                </a>
-              </Link>
-            ))}
+        {isLoading || isRefetching ? (
+          [...Array(6)].map((_, i) => <ProductCardLoading key={i} />)
+        ) : isError ? (
+          <ErrorProduct />
+        ) : (
+          products?.map((v, i) => (
+            <Link href={`/product/${v.id}`} key={i}>
+              <a key={i}>
+                <ProductCard
+                  img={v.image}
+                  price={prices[i]}
+                  rating={String(v.rating.rate)}
+                  title={v.title}
+                />
+              </a>
+            </Link>
+          ))
+        )}
       </div>
     </>
   )
