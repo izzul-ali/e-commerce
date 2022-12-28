04b8ae1a900088f5623986ce57f9a0500fb19eee
izzul-ali <izzulmaali3@gmail.com>
2022-12-28 21:32:37 +0700

    fix: useless logic

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -2,14 +2,14 @@ export const formatCurrency = (usd: number): string => {
   // currency value when it was 12/28/23
   const idr = 15677.55
 
-  if (Number.isNaN(usd)) {
-    return 'Rp 0'
-  }
-
   const usdToIdr = new Intl.NumberFormat('id-ID', {
     style: 'currency',
     currency: 'IDR'
   }).format(usd * idr)
+
+  if (usdToIdr.match('NaN') || String(usd)[0] === '0') {
+    return 'Rp 0,-'
+  }
 
   return usdToIdr
 }
