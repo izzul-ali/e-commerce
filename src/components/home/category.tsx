b15a9ea45dc150732e593d49cbddbb3eeea28cd6
izzul-ali <izzulmaali3@gmail.com>
2022-12-30 10:05:32 +0700

    rename folder

--- src/components/home/category.tsx
+++ src/components/home/category.tsx
@@ -10,25 +10,25 @@ export default function Features() {
     [&>div>p]:text-gray-600 [&>div>p]:text-sm [&>div]:space-y-1 mt-10 flex items-center justify-evenly"
     >
       <div>
-        <button className="p-3 bg-violet-400/50 rounded-lg flex justify-center items-center outline-none">
+        <button className="p-3 bg-violet-400/50 rounded-lg flex justify-center items-center">
           <BiTrendingUp className="w-5 h-fit fill-violet-800" />
         </button>
-        <p>Trends</p>
+        <p>Trend</p>
       </div>
       <div>
-        <button className="p-3 bg-orange-400/30 rounded-lg flex justify-center items-center outline-none">
+        <button className="p-3 bg-orange-400/30 rounded-lg flex justify-center items-center">
           <BiHealth className="w-5 h-fit fill-orange-400" />
         </button>
-        <p>Health</p>
+        <p>Kesehatan</p>
       </div>
       <div>
-        <button className="p-3 bg-sky-400/30 rounded-lg flex justify-center items-center outline-none">
+        <button className="p-3 bg-sky-400/30 rounded-lg flex justify-center items-center">
           <MdDevicesOther className="w-5 h-fit fill-sky-600" />
         </button>
-        <p>Technology</p>
+        <p>Teknologi</p>
       </div>
       <div>
-        <button className="p-3 bg-lime-400/30 rounded-lg flex justify-center items-center outline-none">
+        <button className="p-3 bg-lime-400/30 rounded-lg flex justify-center items-center">
           <GiShirt className="w-5 h-fit fill-lime-600" />
         </button>
         <p>Fashion</p>
