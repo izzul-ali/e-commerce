d5b8294027206148f2323b907cd33e75b474230a
izzul-ali <izzulmaali3@gmail.com>
2022-12-30 10:11:08 +0700

    feat: change box size and error message

--- src/components/fallback/error-product.tsx
+++ src/components/fallback/error-product.tsx
@@ -2,11 +2,11 @@ import { BiErrorCircle } from 'react-icons/bi'
 
 export default function ErrorProduct() {
   return (
-    <div className="h-56 w-full col-span-2 bg-white flex flex-col items-center justify-center text-red-500 rounded gap-y-1">
+    <div className="h-56 w-full col-span-3 bg-white flex flex-col items-center justify-center text-red-500 rounded gap-y-1">
       <span>
-        <BiErrorCircle className="w-8 h-fit" />
+        <BiErrorCircle className="w-7 h-fit" />
       </span>
-      <p className="text-base">Failed to load products</p>
+      <p className="text-base text-center">Gagal memuat daftar produk</p>
     </div>
   )
 }
