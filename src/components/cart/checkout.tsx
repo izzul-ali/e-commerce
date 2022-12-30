6d7a7833d71c6bcb50aa8b4d41bc4efe546d7cf4
izzul-ali <izzulmaali3@gmail.com>
2022-12-30 10:39:45 +0700

    feat: change the checkout button size

--- src/components/cart/checkout.tsx
+++ src/components/cart/checkout.tsx
@@ -5,7 +5,7 @@ export default function Checkout() {
   const totalPrice = useCart((state) => state.totalPrice)
 
   return (
-    <div className="fixed right-0 left-0 bottom-0 pb-5 pt-2 flex justify-between items-center bg-white px-5 z-10">
+    <div className="fixed right-0 left-0 sm:right-20 md:right-32 lg:right-64 sm:left-20 md:left-32 lg:left-64 bottom-0 pb-5 pt-2 flex justify-between lg:justify-around items-center bg-white px-5 z-10">
       <div>
         <p>Total</p>
         <p>{formatCurrency(totalPrice)}</p>
@@ -14,7 +14,7 @@ export default function Checkout() {
         onClick={() => console.log('first')}
         disabled={totalPrice.toFixed() === '0'}
         className={`${
-          totalPrice.toFixed() === '0' ? 'opacity-80' : 'opacity-100'
+          totalPrice.toFixed() === '0' ? 'opacity-70' : 'opacity-100'
         } text-white px-5 py-3 rounded-md text-sm bg-violet-600`}
       >
         Checkout
