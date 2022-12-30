1bc213357f845468d1cd5b54dfb018cd17ff355d
izzul-ali <izzulmaali3@gmail.com>
2022-12-30 10:09:35 +0700

    feat: added page not found

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="h-[70vh] text-gray-700 flex flex-col gap-y-2 justify-center items-center">
+      <h1 className="text-6xl md:text-7xl font-bold">404</h1>
+      <p>Halaman tidak ditemukan</p>
+    </div>
+  )
+}
