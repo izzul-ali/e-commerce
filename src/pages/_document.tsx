caca7b087e190f4513837553b119febbd056d3b8
izzul-ali <izzulmaali3@gmail.com>
2023-01-05 17:01:04 +0700

    feat: added apple-touch-icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,13 @@ export default function Document() {
   return (
     <Html lang="id">
       <Head>
+        <meta name="theme-color" content="#7c3aed" />
         <meta
           name="description"
           content="eksperimen membuat aplikasi e-commerce sederhana yang dibuat dengan next.js dan zustand sebagai state management"
         />
-        <meta name="theme-color" content="#7c3aed" />
         <link rel="icon" type="image/png" href="/icons/favicon.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
