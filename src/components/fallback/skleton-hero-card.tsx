d90e5f9eea20860a597c4075ad1e31e5a7684399
izzul-ali <izzulmaali3@gmail.com>
2023-01-05 16:59:59 +0700

    feat: change p element to button element

--- src/components/fallback/skleton-hero-card.tsx
+++ src/components/fallback/skleton-hero-card.tsx
@@ -5,7 +5,7 @@ export default function HeroCardLoading() {
       <div className="">
         <h3 className="text-lg mb-2 font-medium line-clamp-3 py-2 w-full bg-gray-300 animate-pulse rounded-sm"></h3>
         <h3 className="text-lg font-medium line-clamp-3 py-2 w-full bg-gray-300 animate-pulse rounded-sm"></h3>
-        <button className="mt-5 px-5 py-2 rounded bg-gray-300 animate-pulse"></button>
+        <h3 className="mt-5 px-5 py-2 rounded bg-gray-300 animate-pulse"></h3>
       </div>
     </div>
   )
